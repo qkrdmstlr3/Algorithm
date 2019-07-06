6b89bae0b25bedc3cf0fe04c56a6ce796a21da77
스 <qkrdmstlr3@naver.com>
2019-07-06 10:21:25 +0000

    1. 시간 너무 걸려서 직접 계산에서 끝자리 계산으로 변경 2. 0인 경우와 b가 0일 때를 간과해서 수정 3. commit 자주 하기;;

---
+++ 1009/1009.py
@@ -0,0 +1,24 @@
+"""
+    Author : ParkEunsik
+    Date   : 2019/07/06
+    url    : https://www.acmicpc.net/problem/1009
+"""
+T = int(input())
+for i in range(T):
+    a, b = list(map(int, input().split()))
+    a %= 10
+    if b == 0:
+        print(1)
+        continue
+    elif a == 0:
+        print(10)
+        continue
+    temp = a
+    li = []
+    while True:
+        if a in li:
+            break
+        else:
+            li.append(a)
+        a = (a * temp) % 10
+    print(li[b % len(li) - 1])
