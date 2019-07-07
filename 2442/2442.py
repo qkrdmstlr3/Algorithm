da34cd3c5359340dadc2ed971d08de9f66e1e9ab
스 <qkrdmstlr3@naver.com>
2019-07-07 13:21:20 +0000

    boj_2442

---
+++ 2442/2442.py
@@ -0,0 +1,10 @@
+"""
+    Author : ParkEunsik
+    Date   : 2019/07/07
+    url    : https://www.acmicpc.net/problem/2442
+"""
+N = int(input())
+count = 1
+for i in range(N):
+    print(' '*(N-i-1), '*'*count, sep='')
+    count += 2
