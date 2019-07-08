6f63acd9912c95de442e72ad7fc533c4b7925e61
스 <qkrdmstlr3@naver.com>
2019-07-08 14:19:18 +0000

    boj_15667

---
+++ 15667/15667.py
@@ -0,0 +1,9 @@
+"""
+    Author : ParkEunsik
+    Date   : 2019/07/08
+    url    : https://www.acmicpc.net/problem/15667
+"""
+
+K = int(input())
+D = int((4*K-3)**0.5)
+print(int((-1+(4*K-3)**0.5)/2))
