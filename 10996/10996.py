4d3ce487c8a8b756faadcd1a9f70e2ecdac46283
스 <qkrdmstlr3@naver.com>
2019-07-10 13:52:06 +0000

    boj_10996_&사용

---
+++ 10996/10996.py
@@ -0,0 +1,14 @@
+"""
+    Author : ParkEunsik
+    Date   : 2019/07/08
+    url    : https://www.acmicpc.net/problem/10996
+"""
+N = int(input())
+
+for i in range(2*N):
+    for j in range(N):
+        if (i & 1) == (j & 1):  # 짝수면 0이고 홀수면 1이 됨
+            print("*", end='')
+        else:
+            print(" ", end='')
+    print()
