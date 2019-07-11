ff71a8aaadcff4b6ca4635eaf16786f27521931b
스 <qkrdmstlr3@naver.com>
2019-07-11 14:15:01 +0000

    boj_10817

---
+++ 10817/10817.py
@@ -0,0 +1,9 @@
+"""
+    Author : ParkEunsik
+    Date   : 2019/07/10
+    url    : https://www.acmicpc.net/problem/10817
+    Correct percentage : 47.137%
+"""
+num = list(map(int, input().split()))
+num.sort()
+print(num[1])
