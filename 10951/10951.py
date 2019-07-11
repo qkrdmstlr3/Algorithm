3c1ae9bcbdefe8f4884b44aad305eeb0433cddc4
스 <qkrdmstlr3@naver.com>
2019-07-11 13:43:44 +0000

    boj_10951_예외처리

---
+++ 10951/10951.py
@@ -0,0 +1,12 @@
+"""
+    Author : ParkEunsik
+    Date   : 2019/07/10
+    url    : https://www.acmicpc.net/problem/9012
+    Correct percentage : 36.349%
+"""
+while True:
+    try:
+        num = list(map(int, input().split()))
+        print(num[0]+num[1])
+    except:
+        break;
