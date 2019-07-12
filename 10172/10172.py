e9e64d4aa036025a75c41d7dc60cc543f7957269
스 <qkrdmstlr3@naver.com>
2019-07-12 10:54:38 +0000

    boj_10172

---
+++ 10172/10172.py
@@ -0,0 +1,11 @@
+"""
+    Author : ParkEunsik
+    Date   : 2019/07/12
+    url    : https://www.acmicpc.net/problem/10172
+    Correct percentage : 37.220%
+"""
+print("|\_/|")
+print("|q p|   /}")
+print("( 0 )\"\"\"\\")
+print("|\"^\"`    |")
+print("||_/=\\\__|")
