196e8bb14575572c4a38dab411a60a6977b8c0da
스 <qkrdmstlr3@naver.com>
2019-07-12 11:01:33 +0000

    boj_8393

---
+++ 8393/8393.py
@@ -0,0 +1,9 @@
+"""
+    Author : ParkEunsik
+    Date   : 2019/07/12
+    url    : https://www.acmicpc.net/problem/8393
+"""
+N, num = int(input()), 0
+for i in range(N+1):
+    num += i
+print(num)
