9f43c7c056c61d12c809dd6e2829777531b9fcc7
스 <qkrdmstlr3@naver.com>
2019-07-12 10:57:55 +0000

    boj_2739

---
+++ 2739/2739.py
@@ -0,0 +1,8 @@
+"""
+    Author : ParkEunsik
+    Date   : 2019/07/12
+    url    : https://www.acmicpc.net/problem/2739
+"""
+N = int(input())
+for i in range(1, 10):
+    print(N, "*", i, "=", N*i)
