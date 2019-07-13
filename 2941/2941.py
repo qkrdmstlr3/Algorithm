89105e1fb09deab1893a91e852fed3b786dfd25b
스 <qkrdmstlr3@naver.com>
2019-07-13 07:50:02 +0000

    boj_2941

---
+++ 2941/2941.py
@@ -0,0 +1,15 @@
+"""
+    Author : ParkEunsik
+    Date   : 2019/07/13
+    url    : https://www.acmicpc.net/problem/2941
+"""
+import sys
+
+li, cnt = sys.stdin.readline(), 0
+alphabet = ['c=', 'c-', 'dz=', 'd-', 'lj', 'nj', 's=', 'z=']
+for i in range(len(li)-1):
+    if li[i]+li[i+1] in alphabet:
+        cnt += 1
+    if li[i]+li[i+1] == 'dz' and li[i+1]+li[i+2] == 'z=':
+        cnt += 1
+print(len(li)-cnt-1)
