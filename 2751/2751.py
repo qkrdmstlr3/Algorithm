161b497da83cad6b015fc5ca6735b98d76b8538d
스 <qkrdmstlr3@naver.com>
2019-07-16 11:25:19 +0000

    boj_2751_파이썬으로 정렬은 빡세다!!

--- 2751/2751.py
+++ 2751/2751.py
@@ -2,44 +2,23 @@
     Author : ParkEunsik
     Date   : 2019/07/16
     url    : https://www.acmicpc.net/problem/2751
+    10989코드를 개조함
 """
 
 import sys
 
-
-def merge_sort(li):
-    if len(li) <= 1:
-        return li
-    center = len(li) // 2
-    left = li[:center]
-    right = li[center:]
-    left = merge_sort(left)
-    right = merge_sort(right)
-    return merge(left, right)
-
-
-def merge(left, right):
-    result = []
-    while len(left) > 0 or len(right) > 0:
-        if len(left) != 0 and len(right) != 0:
-            if left[0] > right[0]:
-                result.append(right[0])
-                right = right[1:]
-            else:
-                result.append(left[0])
-                left = left[1:]
-        elif len(left) != 0:
-            result.append(left[0])
-            left = left[1:]
-        elif len(right) != 0:
-            result.append(right[0])
-            right = right[1:]
-    return result
-
-
-li = []
-for i in range(int(input())):
-    li.append(int(sys.stdin.readline()))
-li = merge_sort(li)
-for i in range(len(li)):
-    print(li[i])
+result = [0]*2000002  # 1000001 이전은 음수 이후는 양수 구간
+max, min = 0, 2000003  # 너무 많은 반복 방지 위함
+for i in range(int(sys.stdin.readline())):
+    num = int(sys.stdin.readline())
+    if num > 0:
+        result[num+1000000] += 1
+    else:
+        result[num+1000000] += 1
+    if num > max:
+        max = num
+    if num < min:
+        min = num
+for i in range(min+1000000, max+1000001):
+    for j in range(result[i]):
+        print(i-1000000)
