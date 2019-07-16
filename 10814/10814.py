411c463dde45266bcf2aaf258e2389252ceaa32f
스 <qkrdmstlr3@naver.com>
2019-07-16 14:27:47 +0000

    boj_10814

---
+++ 10814/10814.py
@@ -0,0 +1,18 @@
+"""
+    Author : ParkEunsik
+    Date   : 2019/07/16
+    url    : https://www.acmicpc.net/problem/10824
+"""
+
+import sys
+from operator import itemgetter
+
+boj = []
+for i in range(int(sys.stdin.readline())):
+    li = list(sys.stdin.readline().split())
+    li[0] = int(li[0])
+    boj.append(li)
+
+boj.sort(key=itemgetter(0))
+for i in range(len(boj)):
+    print(boj[i][0], boj[i][1])
