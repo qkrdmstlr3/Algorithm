d47d9b898f5fbeeea8aad12c1aecd96631b82cb8
스 <qkrdmstlr3@naver.com>
2019-07-16 11:11:18 +0000

    boj_11651_리스트내의 리스트 인덱스로 정렬_★

--- 11651/11651.py
+++ 11651/11651.py
@@ -1,7 +1,7 @@
 """
     Author : ParkEunsik
     Date   : 2019/07/15
-    url    : https://www.acmicpc.net/problem/1107
+    url    : https://www.acmicpc.net/problem/11651
 """
 
 import sys
@@ -11,6 +11,6 @@ coor = []
 for i in range(int(sys.stdin.readline())):
     coor.append(list(map(int, sys.stdin.readline().split())))
 
-coor.sort(key=itemgetter(1, 0))
+coor.sort(key=itemgetter(1, 0))  # 우선적으로 y정렬하고 그 다음 x 정렬
 for i in range(len(coor)):
     print(coor[i][0], coor[i][1])
