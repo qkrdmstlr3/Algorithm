ddd6884d544a2f46eb5066328732f5afd54a3787
스 <qkrdmstlr3@naver.com>
2019-07-18 11:48:00 +0000

    boj_11718

--- 11718/11718.py
+++ 11718/11718.py
@@ -1,7 +1,7 @@
 """
     Author : ParkEunsik
     Date   : 2019/07/18
-    url    : https://www.acmicpc.net/problem/2309
+    url    : https://www.acmicpc.net/problem/11718
     correct percentage : 25.271%
 """
 
