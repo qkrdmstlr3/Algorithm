d1968b3e664ab69ee75123b11f395438443ca418
스 <qkrdmstlr3@naver.com>
2019-07-18 11:46:38 +0000

    boj_11718

---
+++ 11718/11718.py
@@ -0,0 +1,12 @@
+"""
+    Author : ParkEunsik
+    Date   : 2019/07/18
+    url    : https://www.acmicpc.net/problem/2309
+    correct percentage : 25.271%
+"""
+
+for i in range(100):
+    try:
+        print(input())
+    except:
+        break
