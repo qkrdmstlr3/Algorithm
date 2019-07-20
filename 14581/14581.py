79c2e49fc10453054fb8f0c3058b48dd22c13336
스 <qkrdmstlr3@naver.com>
2019-07-20 06:24:54 +0000

    boj_14581

---
+++ 14581/14581.py
@@ -0,0 +1,14 @@
+"""
+    Author : ParkEunsik
+    Date   : 2019/07/20
+    url    : https://www.acmicpc.net/problem/14581
+"""
+
+
+import sys
+
+hj = sys.stdin.readline().split()
+
+print(":fan::fan::fan:")
+print(":fan::", hj[0], "::fan:", sep='')
+print(":fan::fan::fan:")
