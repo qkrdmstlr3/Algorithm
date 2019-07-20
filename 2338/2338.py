2d78fc1c26e9f39090a1dee2d7930169cb1984a3
스 <qkrdmstlr3@naver.com>
2019-07-20 13:46:23 +0000

    boj_2338

---
+++ 2338/2338.py
@@ -0,0 +1,14 @@
+"""
+    Author : ParkEunsik
+    Date   : 2019/07/20
+    url    : https://www.acmicpc.net/problem/2338
+    파이썬은 긴자리 계산 쉽게 가능
+"""
+import sys
+
+A = int(sys.stdin.readline())
+B = int(sys.stdin.readline())
+
+print(A+B)
+print(A-B)
+print(A*B)
