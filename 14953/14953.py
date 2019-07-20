3bb8cbf3c649f31fc0d552182f32d2e31ee3c9f4
스 <qkrdmstlr3@naver.com>
2019-07-20 05:29:05 +0000

    boj_14953

---
+++ 14953/14953.py
@@ -0,0 +1,10 @@
+"""
+    Author : ParkEunsik
+    Date   : 2019/07/20
+    url    : https://www.acmicpc.net/problem/14953
+"""
+
+import sys
+
+N = int(sys.stdin.readline())
+print('FA')  # 모든 수가  이런식으로 하면 한자리수 나옴;;
