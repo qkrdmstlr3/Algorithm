545caffe037f482e590fc9fe3d8b7824f8ee1d74
스 <qkrdmstlr3@naver.com>
2019-07-21 09:23:16 +0000

    boj_11365

---
+++ 11365/11365.py
@@ -0,0 +1,13 @@
+"""
+    Author : ParkEunsik
+    Date   : 2019/07/21
+    url    : https://www.acmicpc.net/problem/11356
+"""
+
+import sys
+
+while True:
+    text = input()
+    if text == "END":
+        break
+    print(text[::-1])
