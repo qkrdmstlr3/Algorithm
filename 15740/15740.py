fc7c07d17ecc40a71f04a060f86a92351433e983
스 <qkrdmstlr3@naver.com>
2019-07-24 14:38:09 +0000

    boj_15740

---
+++ 15740/15740.py
@@ -0,0 +1,10 @@
+"""
+    Author : ParkEunsik
+    Date   : 2019/07/24
+    url    : https://www.acmicpc.net/problem/15740
+"""
+
+import sys
+
+a, b = map(int, sys.stdin.readline().split())
+print(a+b)
