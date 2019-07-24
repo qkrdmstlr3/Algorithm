5bd9450dedcc99e091dc33ce466a98980b6f5519
스 <qkrdmstlr3@naver.com>
2019-07-24 14:07:09 +0000

    boj_5338

---
+++ 5338/5338.py
@@ -0,0 +1,13 @@
+"""
+    Author : ParkEunsik
+    Date   : 2019/07/24
+    url    : https://www.acmicpc.net/problem/5338
+"""
+
+import sys
+
+print("       _.-;;-._")
+print("'-..-'|   ||   |")
+print("'-..-'|_.-;;-._|")
+print("'-..-'|   ||   |")
+print("'-..-'|_.-''-._|")
