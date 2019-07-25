7e37e73bf49f1288548d36f093f3d6538cd696e6
스 <qkrdmstlr3@naver.com>
2019-07-25 04:02:42 +0000

    boj_9316

---
+++ 9316/9316.py
@@ -0,0 +1,10 @@
+"""
+    Author : ParkEunsik
+    Date   : 2019/07/25
+    url    : https://www.acmicpc.net/problem/9316
+"""
+
+import sys
+
+for i in range(int(sys.stdin.readline())):
+    print("Hello World, Judge ", i+1, "!", sep='')
