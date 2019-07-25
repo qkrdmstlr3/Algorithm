4019abec2f92429b9b243c8b1a3da710319c02c2
스 <qkrdmstlr3@naver.com>
2019-07-25 04:29:45 +0000

    boj_15680

---
+++ 15680/15680.py
@@ -0,0 +1,13 @@
+"""
+    Author : ParkEunsik
+    Date   : 2019/07/25
+    url    : https://www.acmicpc.net/problem/15680
+"""
+
+import sys
+
+N = int(sys.stdin.readline())
+if N == 0:
+    print("YONSEI")
+else:
+    print("Leading the Way to the Future")
