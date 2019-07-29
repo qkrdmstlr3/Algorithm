ac8b3ca8710ffdeff54ff02bbee666a1cf4edd1b
스 <qkrdmstlr3@naver.com>
2019-07-29 13:14:24 +0000

    boj_13277

---
+++ 13277/13277.py
@@ -0,0 +1,11 @@
+"""
+    Author : ParkEunsik
+    Date   : 2019/07/29
+    url    : https://www.acmicpc.net/problem/13277
+"""
+
+import sys
+
+a, b = map(int, sys.stdin.readline().split())
+
+print(a*b)
