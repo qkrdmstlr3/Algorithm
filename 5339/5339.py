8d709e4ff41f0141531277f6a1f2614ac5d42e9d
스 <qkrdmstlr3@naver.com>
2019-07-29 11:04:23 +0000

    boj_5339

---
+++ 5339/5339.py
@@ -0,0 +1,18 @@
+"""
+    Author : ParkEunsik
+    Date   : 2019/07/29
+    url    : https://www.acmicpc.net/problem/5339
+"""
+
+import sys
+
+print("     /~\\")
+print("    ( oo|")
+print("    _\=/_")
+print("   /  _  \\")
+print("  //|/.\|\\\\")
+print(" ||  \ /  ||")
+print("============")
+print("|          |")
+print("|          |")
+print("|          |")
