9a2040d87fc36def61dbb37d682a2e3e8fbd1bf0
스 <qkrdmstlr3@naver.com>
2019-08-03 13:30:50 +0000

    boj_12791

--- 12791/12791.py
+++ 12791/12791.py
@@ -7,6 +7,7 @@ import sys
 
 db = [[1967, 'DavidBowie'], [1969, 'SpaceOddity'], [1970, 'TheManWhoSoldTheWorld'], [1971, 'HunkyDory'], [1972, 'TheRiseAndFallOfZiggyStardustAndTheSpidersFromMars'], [1973, 'AladdinSane'], [1973, 'PinUps'], [1974, 'DiamondDogs'], [1975, 'YoungAmericans'], [1976, 'StationToStation'], [1977, 'Low'], [1977, 'Heroes'], [1979, 'Lodger'], [1980, 'ScaryMonstersAndSuperCreeps'], [1983, 'LetsDance'], [1984, 'Tonight'], [1987, 'NeverLetMeDown'], [1993, 'BlackTieWhiteNoise'], [1995, '1.Outside'], [1997, 'Earthling'], [1999, 'Hours'], [2002, 'Heathen'], [2003, 'Reality'], [2013, 'TheNextDay'], [2016, 'BlackStar']]
 
+
 for i in range(int(sys.stdin.readline())):
     count, result = 0, []
     s, e = map(int, sys.stdin.readline().split())
