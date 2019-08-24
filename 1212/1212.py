a4366a85c178d240ca57bae90139dc28ebb32a95
스 <qkrdmstlr3@naver.com>
2019-08-24 13:22:12 +0000

    boj_1212

---
+++ 1212/1212.py
@@ -0,0 +1,11 @@
+"""
+    Author : ParkEunsik
+    Date   : 2019/08/24
+    url    : https://www.acmicpc.net/problem/1212
+    correct percentage : 30.525%
+"""
+
+import sys
+
+print(bin(int(sys.stdin.readline(), 8))[2:])
+# int(a, b) : b진수인 a를 10진수로 변환
