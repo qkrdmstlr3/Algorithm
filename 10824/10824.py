ff9779812127b9628434d10b6e5aae6de67cb384
스 <qkrdmstlr3@naver.com>
2019-08-24 13:06:26 +0000

    boj_10824

---
+++ 10824/10824.py
@@ -0,0 +1,11 @@
+"""
+    Author : ParkEunsik
+    Date   : 2019/08/24
+    url    : https://www.acmicpc.net/problem/10824
+    correct percentage : 37.703%
+"""
+
+import sys
+
+A, B, C, D = sys.stdin.readline().split()
+print(int(A+B)+int(C+D))
