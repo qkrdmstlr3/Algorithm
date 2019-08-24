c8c9362e57d101d3b92ce3a0c33ec3ad2fd62978
스 <qkrdmstlr3@naver.com>
2019-08-24 13:42:44 +0000

    boj_1373

---
+++ 1373/1373.py
@@ -0,0 +1,10 @@
+"""
+    Author : ParkEunsik
+    Date   : 2019/07/23
+    url    : https://www.acmicpc.net/problem/1373
+    correct percentage : 37.794%
+"""
+
+import sys
+
+print(oct(int(sys.stdin.readline(), 2))[2:])
