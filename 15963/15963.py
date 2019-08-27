40c603558eeb2f6d6d3800f03f29398a709d8460
스 <qkrdmstlr3@naver.com>
2019-08-27 13:31:13 +0000

    boj_15963

---
+++ 15963/15963.py
@@ -0,0 +1,13 @@
+"""
+    Author : ParkEunsik
+    Date   : 2019/08/27
+    url    : https://www.acmicpc.net/problem/15963
+"""
+
+import sys
+
+N, M = map(int, sys.stdin.readline().split())
+if N==M:
+    print(1)
+else:
+    print(0)
