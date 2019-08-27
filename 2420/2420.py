2c062decdf208ae25c1cf94284e7dd968bf9e9ba
스 <qkrdmstlr3@naver.com>
2019-08-27 10:15:15 +0000

    boj_2420

---
+++ 2420/2420.py
@@ -0,0 +1,10 @@
+"""
+    Author : ParkEunsik
+    Date   : 2019/08/27
+    url    : https://www.acmicpc.net/problem/2420
+"""
+
+import sys
+
+N, M = map(int, sys.stdin.readline().split())
+print(abs(N-M))
