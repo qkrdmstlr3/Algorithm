cbec52c624845ba6df78e67fa92a8ec96abd2e80
스 <qkrdmstlr3@naver.com>
2019-08-28 10:48:09 +0000

    boj_2745

---
+++ 2745/2745.py
@@ -0,0 +1,10 @@
+"""
+    Author : ParkEunsik
+    Date   : 2019/08/28
+    url    : https://www.acmicpc.net/problem/2745
+"""
+
+import sys
+
+N, B = sys.stdin.readline().split()
+print(int(N, int(B)))
