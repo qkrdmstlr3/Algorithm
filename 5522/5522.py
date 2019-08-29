6e7f28b92bf253c82aabe556cc35c43b502322c5
스 <qkrdmstlr3@naver.com>
2019-08-29 13:23:50 +0000

    boj_5522

---
+++ 5522/5522.py
@@ -0,0 +1,12 @@
+"""
+    Author : ParkEunsik
+    Date   : 2019/08/29
+    url    : https://www.acmicpc.net/problem/5522
+"""
+
+import sys
+
+num_sum = 0
+for i in range(5):
+    num_sum += int(sys.stdin.readline())
+print(num_sum)
