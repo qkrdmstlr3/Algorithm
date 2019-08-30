d669443be14e4ea7bd18e26a05e2d0e13b3677da
스 <qkrdmstlr3@naver.com>
2019-08-30 09:57:29 +0000

    boj_2003

---
+++ 2003/2003.py
@@ -0,0 +1,23 @@
+"""
+    Author : ParkEunsik
+    Date   : 2019/08/30
+    url    : https://www.acmicpc.net/problem/2003
+"""
+
+import sys
+
+N, M = map(int, sys.stdin.readline().split())
+array = list(map(int, sys.stdin.readline().split()))
+count = 0
+index1, index2 = 0, 0
+num_sum = 0
+
+while index2 < N:
+    if sum(array[index1:index2+1]) == M:
+        count += 1
+        index1 += 1
+    elif sum(array[index1:index2+1]) > M:
+        index1 += 1
+    else:
+        index2 += 1
+print(count)
