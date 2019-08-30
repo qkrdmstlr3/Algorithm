d3856dabcd48f6adb0f4ad84da22d4a2676b6a70
스 <qkrdmstlr3@naver.com>
2019-08-30 14:11:09 +0000

    boj_2606

--- 2606/2606.py
+++ 2606/2606.py
@@ -1,25 +1,28 @@
 """
     Author : ParkEunsik
-    Date   : 2019/08/20
+    Date   : 2019/08/30
     url    : https://www.acmicpc.net/problem/2606
+    correct percentage : 41.094%
 """
 
 import sys
+sys.setrecursionlimit(100000)
 
 computer = [[] for i in range(int(sys.stdin.readline())+1)]
-virus = []
+virus = [1]
 
 
 def check_virus(arr):
     while len(arr) != 0:
-        com = arr.pop(0)
+        com = arr.pop()
         if com not in virus:
             virus.append(com)
-            viru(computer[com])
+            check_virus(computer[com])
 
 
 for i in range(int(sys.stdin.readline())):
     a, b = map(int, sys.stdin.readline().split())
     computer[a].append(b)
+    computer[b].append(a)  # 반대로도 추가를 시켜주어야 했다;; 
 check_virus(computer[1])
-print(len(virus))
+print(len(virus)-1)
