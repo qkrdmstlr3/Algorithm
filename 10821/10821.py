914aa57df35481f6ff8e49a6af27343ac411c3da
스 <qkrdmstlr3@naver.com>
2019-09-01 14:37:29 +0000

    boj_10821

---
+++ 10821/10821.py
@@ -0,0 +1,9 @@
+"""
+    Author : ParkEunsik
+    Date   : 2019/09/01
+    url    : https://www.acmicpc.net/problem/10821
+"""
+
+import sys
+
+print(len(list(map(int, sys.stdin.readline().split(',')))))
