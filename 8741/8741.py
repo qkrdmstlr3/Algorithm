e8afaccf295c954d245500509d268cec7fbe7fef
스 <qkrdmstlr3@naver.com>
2019-09-05 10:24:40 +0000

    boj_8741

---
+++ 8741/8741.py
@@ -0,0 +1,13 @@
+"""
+    Author : ParkEunsik
+    Date   : 2019/09/04
+    url    : https://www.acmicpc.net/problem/8741
+    correct percentage : 49.216%
+    규칙 찾으면 쉬움!!
+"""
+
+import sys
+
+k = int(sys.stdin.readline())
+
+print('1'*k+'0'*(k-1))
