94bcdc7228904810e6e0b78719d2da02a5398121
스 <qkrdmstlr3@naver.com>
2019-09-18 13:58:57 +0000

    boj_1247

---
+++ 1247/1247.py
@@ -0,0 +1,19 @@
+"""
+    Author : ParkEunsik
+    Date   : 2019/09/18
+    url    : https://www.acmicpc.net/problem/1247
+"""
+
+import sys
+
+for _ in range(3):
+    N = int(sys.stdin.readline())
+    num = []
+    for _ in range(N):
+        num.append(int(sys.stdin.readline()))
+    if sum(num) > 0:
+        print("+")
+    elif sum(num) < 0:
+        print("-")
+    else:
+        print("0")
