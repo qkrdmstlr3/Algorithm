15d696dc34c756fb196b6c833402a9aa850488da
박은식 <qkrdmstlr3@naver.com>
2021-01-01 02:53:46 +0900

    [feat] programmers 올바른 괄호

---
+++ programmers/올바른괄호.js
@@ -0,0 +1,10 @@
+function solution(s) {
+  let count = 0;
+  for (let i = 0; i < s.length; i++) {
+    s[i] === "(" ? (count += 1) : (count -= 1);
+    if (count < 0) {
+      return false;
+    }
+  }
+  return count === 0;
+}
