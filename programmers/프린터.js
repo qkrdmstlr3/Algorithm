0b1e90dbd2700320be3ced217c8f052445185e41
qkrdmstlr3 <qkrdmstlr3@naver.com>
2022-03-21 16:03:46 +0900

    [refactor] programmers-프린터 업데이트
    - 다른 사람 풀이 추가

--- programmers/프린터.js
+++ programmers/프린터.js
@@ -13,3 +13,25 @@ function solution(priorities, location) {
   }
   return count;
 }
+
+////////////////////
+//  다른 사람의 풀이  //
+////////////////////
+function solution(priorities, location) {
+  var list = priorities.map((t, i) => ({
+    my: i === location,
+    val: t,
+  }));
+  var count = 0;
+  while (true) {
+    var cur = list.splice(0, 1)[0];
+    if (list.some((t) => t.val > cur.val)) {
+      list.push(cur);
+    } else {
+      count++;
+      if (cur.my) return count;
+    }
+  }
+}
+
+// some 함수를 사용함으로써 리스트에 최대숫자를 찾아내는 방법을 사용 -> 좀 더 깔끔하게 구현할 수  있다.
