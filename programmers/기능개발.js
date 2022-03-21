5431a4d86b8a032a82feac2b0d4950914c80373e
qkrdmstlr3 <qkrdmstlr3@naver.com>
2022-03-21 11:17:05 +0900

    [feat] programmers-기능개발 업데이트
    - 다른사람 풀이 추가

--- programmers/기능개발.js
+++ programmers/기능개발.js
@@ -1,6 +1,5 @@
 /**
- * 베스트 풀이법
- * https://programmers.co.kr/learn/courses/30/lessons/42586/solution_groups?language=javascript&type=all
+ * 2020.12.31
  */
 function solution(progresses, speeds) {
   const answer = [];
@@ -24,3 +23,27 @@ function solution(progresses, speeds) {
 
   return answer;
 }
+
+////////////////////
+//  다른 사람의 풀이  //
+////////////////////
+function solution(progresses, speeds) {
+  const days = progresses.map((p, i) => Math.ceil((100 - p[i]) / speeds[i]));
+
+  const answer = [0];
+  let maxDay = days[0];
+  for (let i = 0, j = 0; i < days.length; i += 1) {
+    if (days[i] <= maxDay) {
+      answer[j] += 1;
+    } else {
+      maxDay = days[i];
+      answer[++j] = 1;
+    }
+  }
+
+  return answer;
+}
+
+// map활용해서 계산. 하지만 for문이 더 빠르다고한다
+// for문에서 i, j선언 후 i는 순회에, j는 맘대로 사용
+// 완주하지못한선수처럼 [++j]를 통해서 속성처럼 인덱스를 넣을 수 있다.
